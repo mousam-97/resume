b4fdd6e137984a27e6f33f03c6265415c35376cb
Mousam <mousam.kalita777@gmail.com>
2022-02-19 14:02:13 +0530

    1

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./contact.scss";
 
 function Contact() {
-  return <div className="contact" id="contact">Contact ME - Changed - checking for deployment</div>;
+  return <div className="contact" id="contact">Contact ME - Changed - checking for deployment- continuous deployment</div>;
 }
 
 export default Contact;
